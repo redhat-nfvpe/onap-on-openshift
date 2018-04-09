5dd84b403b066587b1ccac1b10422fd3e9663967
Frank A. Zdarsky <fzdarsky@redhat.com>
2018-04-09 06:40:32 -0400

    Upgrade to OpenShift v3.9.

--- deploy_openshift_aio.sh
+++ deploy_openshift_aio.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
 
-OC_URL=https://github.com/openshift/origin/releases/download/v3.7.1/openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit.tar.gz
-#OC_URL=https://github.com/openshift/origin/releases/download/v3.9.0-alpha.3/openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit.tar.gz
+: "${OC_URL:=https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz}"
+: "${KUBECTL_VERSION:=v1.9.1}"
+: "${HELM_VERSION:=v2.8.2}"
+: "${TILLER_NAMESPACE:=kube-system}"
 OC_TARFILE=$(basename $OC_URL)
 OC_DIR=$(basename -s .tar.gz $OC_URL)
-KUBECTL_VERSION=v1.7.12
-HELM_VERSION=v2.8.1
 HELM_URL=https://kubernetes-helm.storage.googleapis.com/helm-$HELM_VERSION-linux-amd64.tar.gz
 HELM_TARFILE=$(basename $HELM_URL)
-TILLER_NAMESPACE=kube-system
+SCRIPT_DIR=$(pwd)
 
-# install requirements
-echo -e "\n== Installing requirements =="
+title() { echo -e "\E[34m\n== $1 ==\E[00m"; }
+
+title "Installing requirements"
 sudo yum install -y docker
 sudo yum remove -y kubernetes-client
 
 # configure host as described in https://github.com/openshift/origin/blob/master/docs/cluster_up_down.md
-echo -e "\n== Configuring host =="
+title "Configuring host"
 sudo /sbin/sysctl -w net.ipv4.ip_forward=1
 
+echo "Enabling use of insecure Docker registries..."
 echo '{"insecure-registries": ["172.30.0.0/16"] }' | sudo tee /etc/docker/daemon.json > /dev/null
 sudo systemctl daemon-reload
 sudo systemctl enable docker
 sudo systemctl restart docker
 
 if ! sudo firewall-cmd --zone=dockerc --list-all &> /dev/null; then
+    echo "Adding firewall zone 'dockerc' and opening UI and DNS ports..."
     SUBNET=$(sudo docker network inspect -f "{{range .IPAM.Config }}{{ .Subnet }}{{end}}" bridge)
     sudo firewall-cmd --permanent --new-zone dockerc
     sudo firewall-cmd --permanent --zone dockerc --add-source $SUBNET
@@ -34,50 +37,47 @@ if ! sudo firewall-cmd --zone=dockerc --list-all &> /dev/null; then
     sudo firewall-cmd --reload
 fi
 
-# install the OpenShift client tool
-echo -e "\n== Downloading and installing OpenShift client tool =="
+title "Downloading and installing OpenShift client tool"
 cd /tmp
 wget -nv -nc $OC_URL
 tar -xf $OC_TARFILE 2> /dev/null
-sudo cp $OC_DIR/oc /usr/bin/
+sudo cp -- "$OC_DIR/oc" /usr/bin/
 sudo chmod +x /usr/bin/oc
 rm -r $OC_DIR $OC_TARFILE
-cd -
+cd $SCRIPT_DIR
 oc version
 
-# deploy the AIO OpenShift cluster
-echo -e "\n== Deploying OpenShift AIO cluster =="
+title "Deploying OpenShift AIO cluster"
 oc cluster up --service-catalog=true --public-hostname=$(hostname -f)
 
-# log into OpenShift cluster
-echo -e "\n== Logging into the OpenShift AIO cluster =="
+title "Logging into the OpenShift AIO cluster"
 oc login -u system:admin
 
-# install kubectl client
-echo -e "\n== Installing kubectl client $KUBECTL_VERSION =="
+title "Installing kubectl client $KUBECTL_VERSION"
 cd /tmp
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/bin/kubectl
-cd -
+cd $SCRIPT_DIR 
 
-# install Tiller into the cluster
-echo -e "\n== Installing Tiller server $HELM_VERSION =="
-oc project kube-system
-oc process -f https://github.com/openshift/origin/raw/master/examples/helm/tiller-template.yaml -p TILLER_NAMESPACE="${TILLER_NAMESPACE}" -p HELM_VERSION="$HELM_VERSION" | oc create -f -
+title "Installing Tiller server $HELM_VERSION"
+if ! oc project $TILLER_NAMESPACE 2> /dev/null; then
+  oc new-project $TILLER_NAMESPACE
+fi
+oc process -f https://github.com/openshift/origin/raw/master/examples/helm/tiller-template.yaml -p TILLER_NAMESPACE="$TILLER_NAMESPACE" -p HELM_VERSION="$HELM_VERSION" | oc create -f -
 oc rollout status deployment tiller
-oc adm policy add-cluster-role-to-user cluster-admin -z tiller -n ${TILLER_NAMESPACE}
-oc expose deployment/tiller --target-port tiller --type=NodePort --name=tiller -n ${TILLER_NAMESPACE}
+oc adm policy add-cluster-role-to-user cluster-admin -z tiller -n $TILLER_NAMESPACE
+oc expose deployment/tiller --target-port tiller --type=NodePort --name=tiller -n $TILLER_NAMESPACE
 
-# install Helm client
-echo -e "\n== Installing Helm client $HELM_VERSION =="
+title "Installing Helm client $HELM_VERSION"
 cd /tmp
-wget -nv -nc $HELM_URL
+wget -nv $HELM_URL
 tar -xf $HELM_TARFILE 2> /dev/null
-sudo cp linux-amd64/helm /usr/bin/
+sudo rm /usr/bin/helm
+sudo cp linux-amd64/helm /usr/bin/helm
 rm -r linux-amd64 $HELM_TARFILE
-cd -
-helm init --client-only
+cd $SCRIPT_DIR
+helm init --client-only --tiller-namespace=$TILLER_NAMESPACE
 export HELM_HOME
 helm version
 
--- undeploy_openshift_aio.sh
+++ undeploy_openshift_aio.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-echo -e "\n== Undeploy OpenShift AIO =="
+title() { echo -e "\E[34m\n== $1 ==\E[00m"; }
+
+title "Undeploying OpenShift AIO"
 oc cluster down
+rm -rf ~/.kube ~/.helm
+echo "Done."
 
